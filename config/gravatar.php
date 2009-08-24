3874f2d1bc900face6bfeb6954184a802ccefba9
Sam Clark <sam@clark.name>
2009-08-24 20:53:36 +0100

    Added the Gravatar config and class files. Refactored for Kohana 3

---
+++ config/gravatar.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'default' => array(
+		'service'           => 'http://www.gravatar.com/avatar.php', // The gravatar service URL
+		'default_image'     => FALSE,        // The default image if Gravatar is not found, FALSE uses Gravatar default
+		'size'              => 100,                                  // The size of the returned gravatar
+		'view'              => 'gravatar/image',                     // The default view
+		'rating'            => Gravatar::GRAVATAR_G,                 // The default rating
+		'alt'               => FALSE,                                // Alternate image string, FALSE to omit, string to include
+	)
+);
