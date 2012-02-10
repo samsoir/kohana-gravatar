550954cb717c6f521d950ae94255727559ead137
John Chipps-Harding <digitaljohn@me.com>
2012-02-10 16:12:24 +0000

    Fixed config loading issue
    Signed-off-by: John Chipps-Harding <digitaljohn@me.com>

--- classes/kohana/gravatar.php
+++ classes/kohana/gravatar.php
@@ -96,17 +96,17 @@ class Kohana_Gravatar {
 		elseif (is_array($config))
 		{
 			// Setup the configuration
-			$config += Kohana::config('gravatar.default');
+			$config += Kohana::$config->load('gravatar.default');
 			$this->_config = $config;
 		}
 		elseif (is_string($config))
 		{
-			if ($config = Kohana::config('gravatar.'.$config) === NULL)
+			if ($config = Kohana::$config->load('gravatar.'.$config) === NULL)
 			{
 				throw new Kohana_Gravatar_Exception('Gravatar.__construct() , Invalid configuration group name : :config', array(':config' => $config));
 			}
 
-			$this->_config = $config + Kohana::config('gravatar.default');
+			$this->_config = $config + Kohana::$config->load('gravatar.default');
 		}
 	}
 
